dfa266c76fc8d66386108421446e997dd833014c
amaan16994-collab <amaan16994@gmail.com>
2026-09-14 09:44:29 +0500

    Delete Q4.cpp

--- Q4.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    float temperature;
-    printf("Enter temperature: ");
-    scanf("%f", &temperature);
-    printf("Temperature: %.2f\n", temperature);
-    return 0;
-}
